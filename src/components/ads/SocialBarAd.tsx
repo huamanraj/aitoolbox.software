74caffe808f5a023020706ca52f3e87adbed4052
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2025-08-17 01:47:01 +0530

    adult ad off

--- src/components/ads/SocialBarAd.tsx
+++ src/components/ads/SocialBarAd.tsx
@@ -6,7 +6,7 @@ const SocialBarAd: FC = () => {
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
-      "//pl27414897.profitableratecpm.com/a9/f8/a5/a9f8a580a887c25e76e1df446a4ad344.js";
+      "//bureaubittercomic.com/a9/f8/a5/a9f8a580a887c25e76e1df446a4ad344.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
